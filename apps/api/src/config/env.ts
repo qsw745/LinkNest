0bd0b48a3b3474b4fa5303480d3a1ac1fa02dd2a
qsw <2352091806@qq.com>
2025-11-24 16:07:18 +0800

    修改请求地址

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config({ path: process.env.API_ENV_PATH || '.env' })
 
 export const env = {
+  host: process.env.HOST || '0.0.0.0',
   port: Number(process.env.PORT || 4000),
   jwtSecret: process.env.JWT_SECRET || 'linknest-dev-secret',
   db: {
